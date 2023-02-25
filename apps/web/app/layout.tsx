8a88e1dbceb167513f15d8ffdf5cc5f9e1b1c694
Alex Kondratyuk <lynxtaa@gmail.com>
2023-02-25 21:42:37 +0100

    chore: update deps

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,31 @@
+import { Metadata } from 'next'
 import '../styles/global.css'
+
+export const metadata: Metadata = {
+	title: 'Where To Go',
+	description: 'Where To Go',
+	icons: {
+		apple: '/apple-touch-icon.png',
+		icon: [
+			{
+				type: 'image/png',
+				sizes: '32x32',
+				url: '/favicon-32x32.png',
+			},
+			{
+				type: 'image/png',
+				sizes: '16x16',
+				url: '/favicon-16x16.png',
+			},
+		],
+	},
+	manifest: '/site.webmanifest',
+	openGraph: {
+		title: 'Where To Go',
+		description: 'Where To Go',
+		images: [{ url: '/android-chrome-512x512.png' }],
+	},
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
