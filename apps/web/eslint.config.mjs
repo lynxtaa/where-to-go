d0347921b96bcfcdff4408d6ffe10b7a536d3334
Alex Kondratiuk <lynxtaa@gmail.com>
2025-12-06 18:13:35 +0100

    chore: update dependencies (#3)

---
+++ apps/web/eslint.config.mjs
@@ -0,0 +1,23 @@
+import eslintConfig from '@lynxtaa/eslint-config'
+import requiresTypechecking from '@lynxtaa/eslint-config/requires-typechecking'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import next from 'eslint-config-next'
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
+
+export default defineConfig([
+	...next,
+	...nextCoreWebVitals,
+	...eslintConfig,
+	...requiresTypechecking,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: {
+					allowDefaultProject: ['*.js', '*.mjs'],
+				},
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts', 'coverage']),
+])
